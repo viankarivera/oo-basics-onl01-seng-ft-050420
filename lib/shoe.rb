e9d0c2bdff6aa2213563d6959b5e99a9370fa18c
Vianka Rivera <viankalrivera@gmail.com>
2020-05-11 16:28:08 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,4 @@
 # Make your shoe class here!
 class Shoe
- def initialize(brand)
-   @brand = brand
- end
- def brand = (brand)
-   @brand = brand
- end
- def brand
-   @brand
- end
-end
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
